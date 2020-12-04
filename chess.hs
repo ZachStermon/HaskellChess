9bb49b4f2faa2b1933939629d490611def1cb832
Jacob Swisher <jacobswisher853@gmail.com>
2020-12-04 17:31:55 -0500

    added main functionality and some valid move checking

--- chess.hs
+++ chess.hs
@@ -5,18 +5,18 @@ module Chess
 , setspotonboard , getspotonboard
 , executemove , validmove
 , format , split
-, initial
+, initial , stringtomove, getbotmove
 , Color , Type , Piece , Spot , Board , Move
 ) where
 
-data Color = White | Black deriving (Show)
-data Type = Pawn | Knight | Bishop | Rook | Queen | King deriving (Show)
-data Piece = Piece Type Color deriving (Show)
+data Color = White | Black deriving (Show, Eq)
+data Type = Pawn | Knight | Bishop | Rook | Queen | King deriving (Show, Eq)
+data Piece = Piece Type Color deriving (Show, Eq)
 type Spot = Maybe Piece
 type Board = [Spot]
 type Move = (Int, Int)
-type Player = {Name :: String, Age :: Int, ELO :: Int}
-type Game = {W :: Player, B :: Player}
+data Player = Player {name :: String, age :: Int} deriving (Show)
+data Game = Game {w :: Player, b :: Player} deriving (Show)
 
 initial = getboard(init_)
 init_ = init1 ++ init2 ++ init3 ++ init3 ++ init3 ++ init3 ++ init4 ++ init5
@@ -31,13 +31,26 @@ DONE
   formatted printing Board
   moving pieces
   updating board
-TODO
-  ****fix main****
-  **valid move checking**
-  *IO prompts*
-  alternate turns
-  Random bot??
   type player
+  IO prompts
+  alternate turns
+  fix main
+TODO
+  **valid move checking**
+  Random bot??
+-}
+
+{-
+board representation is a single vector:
+8 | 0  1  2  3  4  5  6  7  | 8
+7 | 8  9  10 11 12 13 14 15 | 7
+6 | 16 17 18 19 20 21 22 23 | 6
+5 | 24 25 26 27 28 29 30 31 | 5
+4 | 32 33 34 35 36 37 38 39 | 4
+3 | 40 41 42 43 44 45 46 47 | 3
+2 | 48 49 50 51 52 53 54 55 | 2
+1 | 56 57 58 59 60 61 62 63 | 1
+    A  B  C  D  E  F  G  H
 -}
 
 showpiece :: Piece -> Char
@@ -69,10 +82,32 @@ getpiece 'q' = Just (Piece Queen Black)
 getpiece 'k' = Just (Piece King Black)
 getpiece  _  = Nothing
 
-{-
-getMove :: String -> Move
-getMove [] =
--}
+
+ctr :: Char -> Int
+ctr x = abs ((read [x]) - 8)
+
+ctc :: Char -> Int
+ctc 'a' = 0
+ctc 'b' = 1
+ctc 'c' = 2
+ctc 'd' = 3
+ctc 'e' = 4
+ctc 'f' = 5
+ctc 'g' = 6
+ctc 'h' = 7
+ctc 'A' = 0
+ctc 'B' = 1
+ctc 'C' = 2
+ctc 'D' = 3
+ctc 'E' = 4
+ctc 'F' = 5
+ctc 'G' = 6
+ctc 'H' = 7
+
+--input will look like a2 c6
+
+stringtomove :: String -> Move
+stringtomove s = (ctr (s!!1) * 8 + ctc (s!!0), ctr (s!!3) * 8 + ctc (s!!2))
 
 showspot :: Spot -> Char
 showspot Nothing = '-'
@@ -98,13 +133,59 @@ getspotonboard (xs) i = xs !! i
 executemove :: Board -> Move -> Board
 executemove b m = setspotonboard (setspotonboard b (getspotonboard b (fst m)) (snd m)) (getspot '-') (fst m)
 
-validmove :: Board -> Move -> Bool
-validmove _ _ = True
+inrange :: Move -> Bool
+inrange x = ((fst x >= 0 && fst x <= 63) && (snd x >= 0 && snd x <= 63))
+
+exists :: Board -> Move -> Bool
+exists b m = not (showspot (b !! (fst m)) == '-')
+
+changed :: Move -> Bool
+changed m = not(fst x == snd x)
+
+notfriendlyfire :: Board -> Move -> Bool
+notfriendlyfire b m = not ((getspotonboard b (fst m)) == (getspotonboard b (snd m)))
+
+correctturn :: Board -> Move -> Bool -> Bool
+correctturn b m True
+correctturn b m False
+
+{-
+* check if O and D are in range 0-63
+* check if O is empty/null
+check if its the correct for the person moving
+* check if O==D
+check if friendly fire is happening
+
+*test if a piece is in the way (not for knights)*
+***test movement type is right***
+-}
+
+validmove :: Board -> Move -> Bool -> Bool
+validmove b m t = (inrange m) && (exists b m) && (correctturn b m t) && (changed m) && notfriendlyfire (b m)
+
+validmove :: Board -> Move -> Spot -> Bool
+validmove b m (Just (Piece Pawn White))   = True
+validmove b m (Just (Piece Pawn Black))   = True
+validmove b m (Just (Piece Knight _))     = True
+validmove b m (Just (Piece Bishop _))     = True
+validmove b m (Just (Piece Rook _))       = True
+validmove b m (Just (Piece Queen _))      = True
+validmove b m (Just (Piece King _))       = True
+validmove _ _ _                           = False
+
+getrow :: Int -> Int
+getrow i = quot i 8
+
+getcol :: Int -> Int
+getcol i = mod i 8
+
+
+getbotmove :: Board -> Int -> Int -> Move
+getbotmove b a1 a2 = (a1,a2)
+
 
 format :: String -> [String]
 format s = split 8 s
 
 split :: Int -> [a] -> [[a]]
 split n = takeWhile (not.null) . map (take n) . iterate (drop n)
-
---putStr $ unlines $ format (showboard(executemove initial (9,33)))
--- main.hs
+++ main.hs
@@ -1,20 +1,135 @@
-import chess
+import Chess
 
 --This function creates the initial chess board position
 --Then the function calls the main play function
 main :: IO()
-main = play initial
+main = do
+       putStrLn ""
+       putStrLn "Authors: Jacob Swisher and Zach Stermon"
+       putStrLn "Version: 1.2"
+       putStrLn ""
+       putStrLn "This is a simple chess program written entirely in haskell"
+       putStrLn "To play a move, type in the origin square (eg. `e2`), followed"
+       putStrLn "immediately by the destination square (eg. `e2e4` moves the piece"
+       putStrLn "in e2 to the square e4)"
+       putStrLn ""
+       putStrLn "This program uses FEN ASCII notation to represent chess pieces"
+       putStrLn "Uppercase letters are white, lowercase are black"
+       putStrLn ""
+       putStrLn "If you are still confused, type `I` for more information, otherwise type `N` to select your game type: "
+       moreinfo <- getChar
+       if moreinfo == 'i' || moreinfo == 'I'
+       then info
+       else start
+
+
+
+start :: IO()
+start = do
+            error <- getChar
+            putStrLn "Play computer or another human? (C,H)"
+            gameselection <- getChar
+            if gameselection == 'c' || gameselection == 'C'
+            then playbotsetup
+            else playsetup
+
+info :: IO()
+info = do
+       putStrLn ""
+       putStrLn "More information on FEN notation:"
+       putStrLn "Uppercase letters are white, lowercase are black"
+       putStrLn "P -> White Pawn"
+       putStrLn "N -> White Knight"
+       putStrLn "B -> White Bishop"
+       putStrLn "R -> White Rook"
+       putStrLn "Q -> White Queen"
+       putStrLn "K -> White King"
+       putStrLn "p -> Black Pawn"
+       putStrLn "n -> Black Knight"
+       putStrLn "b -> Black Bishop"
+       putStrLn "r -> Black Rook"
+       putStrLn "q -> Black Queen"
+       putStrLn "k -> Black King"
+       putStrLn ""
+       start
+
+
+
+
+playbotsetup :: IO()
+playbotsetup = do
+               error <- getLine
+               putStrLn "Whats your name?"
+               name_ <- getLine
+               putStrLn "Whats your age?"
+               age_ <- getLine
+               putStrLn "Select difficulty (1-10)"
+               skill_ <- getLine
+               --let player1 = Player name_ age_
+               --let skill = skill_
+               playbot initial True
+
+playsetup :: IO()
+playsetup = do
+          error <- getLine
+          putStrLn "Player 1, Please Enter Your Name: "
+          player1name <- getLine
+          putStrLn "Player 1, Please Enter Your Age: "
+          player1age <- getLine
+          putStrLn "Player 2, Please Enter Your Name: "
+          player2name <- getLine
+          putStrLn "Player 2, Please Enter Your Age: "
+          player2age <- getLine
+          putStrLn $ (player1name ++ " is Player 1 and they are White.")
+          putStrLn $ (player2name ++ " is Player 2 and they are Black.")
+          play initial True
+          --let player1 = Player player1name player1age
+          --let player2 = Player player2name player2age
+
+
 
 --This function is played repeatedly for each players move
-play :: Board -> IO()
-play b = do showboard b
-                putStr "move(ORIGIN DESTINATION): "
+play :: Board -> Bool -> IO()
+play b True = do
+                printboard b
+                putStrLn "It Is Now Whites Turn," --(" ++ player1name ++ ")"
+                putStrLn "Enter valid move (a1b2): "
                 move <- getLine
-                movement b stringtomove(move)
+                movement (b) (stringtomove(move)) False
+play b False = do
+                printboard b
+                putStrLn "It Is Now Blacks Turn, " --(" ++ player2name ++ ")"
+                putStrLn "Enter valid move (a1b2): "
+                move <- getLine
+                movement (b) (stringtomove(move)) True
 
+playbot :: Board -> Bool -> IO()
+playbot b True = do
+                printboard b
+                putStrLn "It Is Now Whites Turn,"
+                putStrLn "Enter valid move (a1b2): "
+                move <- getLine
+                movementbot (b) (stringtomove(move))
+playbot b False = do
+                printboard b
+                playbot (executemove b (1, 2)) True
 
 --This function tests if the move is valid and plays it if it is
-movement :: Board -> Move -> IO()
-movement b m = if validmove b m
-               then putStrLn "Good Move" >> play executemove b m
-               else putStrLn "Bad Move" >> play b
+movement :: Board -> Move -> Bool -> IO()
+movement b m x = if validmove b m (b !! (fst m))
+                 then putStrLn "Good Move" >> play (executemove b m) x
+                 else putStrLn "Bad Move" >> play b (not x)
+
+movementbot :: Board -> Move -> IO()
+movementbot b m = if validmove b m (b !! (fst m))
+               then putStrLn "Good Move" >> playbot (executemove b m) False
+               else putStrLn "Bad Move" >> playbot b True
+
+--prints a board to stdout
+printboard :: Board -> IO()
+printboard b = do
+               putStrLn ""
+               putStrLn "==ABCDEFGH=="
+               putStr $ unlines $ zipWith (++)(zipWith (++) ["8 ","7 ","6 ","5 ","4 ","3 ","2 ","1 "] (format $ (showboard b))) [" 8"," 7"," 6"," 5"," 4"," 3"," 2"," 1"]
+               putStrLn "==ABCDEFGH=="
+               putStrLn ""
